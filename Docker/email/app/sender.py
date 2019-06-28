f8617daaf89734cf8d22ab12b896180e89469f01
Alessandro Luchesi Corrêa <aleluchesi@gmail.com>
2019-06-28 11:06:56 -0300

    Lição 57

--- Docker/email/app/sender.py
+++ Docker/email/app/sender.py
@@ -7,10 +7,10 @@ from bottle import Bottle, request
 
 class Sender(Bottle):
 
-    def _init_(self):
+    def __init__(self):
         super().__init__()
         self.route('/', method='POST', callback=self.send)
-        self.fila = redis.StrictRedis(host=redis_host, port=6379, db=0)
+        self.fila = redis.StrictRedis(host='queue', port=6379, db=0)
 
         DSN = 'dbname=email_sender user=postgres host=db'
 
@@ -41,7 +41,5 @@ class Sender(Bottle):
         )
 
 if __name__ == '__main__':
-    logging.warning(' >>>>>>>> Inicializando Sender.')
     sender = Sender()
-    logging.warning(' >>>>>>>> carregado Sender.')
     sender.run(host='0.0.0.0', port=8080, debug=True)
